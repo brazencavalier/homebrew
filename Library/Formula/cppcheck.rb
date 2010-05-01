efc07d909e58ab71d8c00c33ac1c1c0698411127
Theo Wadsley <brazen.cavalier@gmail.com>
2010-05-01 13:07:17 +1000

    Add new cppcheck formula.
    Cppcheck - A tool for static C/C++ code analysis
    http://sourceforge.net/apps/mediawiki/cppcheck/index.php?title=Main_Page
    
    Doesn't have man pages yet, requires docbook files to generate them.

---
+++ Library/Formula/cppcheck.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Cppcheck < Formula
+  url 'http://github.com/danmar/cppcheck/tarball/1.42'
+  homepage 'http://sourceforge.net/apps/mediawiki/cppcheck/index.php?title=Main_Page'
+  md5 'de8ffbd9d02c4ec01047ef7fd9f208cd'
+
+  def install
+    # Binary install dir defaults to "${DESTDIR}/usr/bin", need to change to fix this.
+    bin = "#{prefix}/bin"
+    # Need to remove "-Wlogical-op" from c++ flags.
+    cxxflags = "-Wall -Wextra -pedantic -Wfloat-equal -Wcast-qual -g -D_GLIBCXX_DEBUG"
+
+    # Pass to make variables.
+    system "make BIN=\"" + bin + "\" CXXFLAGS=\"" + cxxflags + "\" install"
+    # Man pages aren't installed, they require docbook schemas which I don't know how to install.
+  end
+end
